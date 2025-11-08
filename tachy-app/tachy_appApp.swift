74ed181c6637afb251e461d12fe4659a567dda6d
Laksh Bharani <laksh.bharani@gmail.com>
2025-11-08 17:33:45 -0500

    Initial Commit

---
+++ tachy-app/tachy_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tachy_appApp.swift
+//  tachy-app
+//
+//  Created by Laksh Bharani on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct tachy_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
